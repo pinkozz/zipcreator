6209c0fc1bc9aa54e742017c5c4f835d5c54b0bc
H4ZE888 <username@example.com>
2023-06-30 17:55:50 +0300

    UPDATED VERSION

--- main/main.py
+++ main/main.py
@@ -2,6 +2,14 @@
 
 import zipfile
 
-newZip = zipfile.ZipFile('YOUR_ZIP_FILENAME.zip', 'w')
-newZip.write('test', compress_type=zipfile.ZIP_DEFLATED)
+# Test a block of code for errors
+try:
+    # Open zip-file and compress test.txt
+    newZip = zipfile.ZipFile('zipname.zip', 'w')
+    newZip.write('test.txt', compress_type=zipfile.ZIP_DEFLATED)
+# Handle the error
+except Exception as e:
+    print(e)
+
+# Close the file
 newZip.close()
